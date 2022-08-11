b267c3c76113dce62f1fc52774b2b7dd018347c8
Xmuluneh <mulabr673@gmail.com>
2022-08-11 14:18:57 +0300

    First week

---
+++ p.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+num = 10+5
+print(num)
+hometown,hometownw = "DC","Addis Ababa"
+print("Hello world!")
+print("My name is Sisay and I am form ",hometown ,hometownw)
+print("print hello ")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+print(num)
+"print hello "
